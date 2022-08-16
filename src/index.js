2b1102fb3e8b7eb075170d54399886ea17f4215d
Sergey Shishkin <Shishkin_SU@ukr.net>
2022-08-16 15:55:10 +0300

    add lightbox

--- src/index.js
+++ src/index.js
@@ -1,38 +1,32 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 import NewsApiService from './js/components/news-service';
 import LoadMoreBtn from './js/components/load-more-btn';
-
 const refs = {
   searchForm: document.querySelector('#search-form'),
   searchButton: document.querySelector('[data-action="search"]'),
   galleryContainer: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
-
 const loadMoreBtn = new LoadMoreBtn({
   selector: '[data-action="load-more"]',
   hidden: true,
 });
 const newsApiService = new NewsApiService();
 
+let gallery = null;
+
 refs.searchForm.addEventListener('submit', onSearch);
 loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
-
 function onSearch(e) {
   e.preventDefault();
-
   newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
-
   if (newsApiService.query === '') {
     return Notify.failure('Введи что-то нормальное');
   }
-
   newsApiService.resetPage();
   clearGalleryContainer();
-
   newsApiService.fetchGallery().then(data => {
     if (data.hits.length === 0) {
       Notify.failure(
@@ -41,7 +35,12 @@ function onSearch(e) {
     } else {
       Notify.success(`Hooray! We found ${data.totalHits} images`);
       insertContent(data.hits);
-
+      gallery = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+        captionPosition: 'bottom',
+      });
+      console.log(gallery);
       loadMoreBtn.show();
     }
     if (newsApiService.page > Math.ceil(data.total / 40)) {
@@ -49,7 +48,6 @@ function onSearch(e) {
     }
   });
 }
-
 function onLoadMore() {
   newsApiService.fetchGallery().then(data => {
     Notify.success(`Hooray! We found ${data.totalHits} images`);
@@ -60,15 +58,14 @@ function onLoadMore() {
       loadMoreBtn.hide();
     } else {
       insertContent(data.hits);
+      gallery.refresh();
     }
   });
 }
-
 const createListItem = item =>
-  `<div class="photo-card">
-     <a class="gallery__link" href="${item.largeImageURL}  target="_blank"
-          rel="noopener noreferrer nofollow">
-       <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
+  `
+     <a class="gallery__link" href="${item.largeImageURL}">
+       <img src="${item.webformatURL}" alt="${item.tags}"/>
       <div class="info">
         <p class="info-item">
         <b>Likes ${item.likes ? item.likes : ''}</b>
@@ -84,24 +81,121 @@ const createListItem = item =>
        </p>
       </div>
     </a>
-  </div>`;
-
+  `;
 const generateContent = array =>
   array ? array.reduce((acc, item) => acc + createListItem(item), '') : '';
-
 const insertContent = array => {
   const result = generateContent(array);
   refs.galleryContainer.insertAdjacentHTML('beforeend', result);
 };
-
 function clearGalleryContainer() {
   refs.galleryContainer.innerHTML = '';
 }
 
-let gallery = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-  captionPosition: 'bottom',
-});
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-gallery.refresh();
+// import NewsApiService from './js/components/news-service';
+// import LoadMoreBtn from './js/components/load-more-btn';
+
+// const refs = {
+//   searchForm: document.querySelector('#search-form'),
+//   searchButton: document.querySelector('[data-action="search"]'),
+//   galleryContainer: document.querySelector('.gallery'),
+//   loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+// };
+
+// const loadMoreBtn = new LoadMoreBtn({
+//   selector: '[data-action="load-more"]',
+//   hidden: true,
+// });
+// const newsApiService = new NewsApiService();
+
+// refs.searchForm.addEventListener('submit', onSearch);
+// loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
+
+// function onSearch(e) {
+//   e.preventDefault();
+
+//   newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+
+//   if (newsApiService.query === '') {
+//     return Notify.failure('Введи что-то нормальное');
+//   }
+
+//   newsApiService.resetPage();
+//   clearGalleryContainer();
+
+//   newsApiService.fetchGallery().then(data => {
+//     if (data.hits.length === 0) {
+//       Notify.failure(
+//         `Sorry, there are no images matching your search query. Please try again.`
+//       );
+//     } else {
+//       Notify.success(`Hooray! We found ${data.totalHits} images`);
+//       insertContent(data.hits);
+
+//       loadMoreBtn.show();
+//     }
+//     if (newsApiService.page > Math.ceil(data.total / 40)) {
+//       loadMoreBtn.hide();
+//     }
+//   });
+// }
+
+// function onLoadMore() {
+//   newsApiService.fetchGallery().then(data => {
+//     Notify.success(`Hooray! We found ${data.totalHits} images`);
+//     if (newsApiService.page > Math.ceil(data.total / 40)) {
+//       Notify.success(
+//         `We're sorry, but you've reached the end of search results.`
+//       );
+//       loadMoreBtn.hide();
+//     } else {
+//       insertContent(data.hits);
+//     }
+//   });
+// }
+
+// const createListItem = item =>
+//   `<div class="photo-card">
+//      <a class="gallery__link" href="${item.largeImageURL}  target="_blank"
+//           rel="noopener noreferrer nofollow">
+//        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
+//       <div class="info">
+//         <p class="info-item">
+//         <b>Likes ${item.likes ? item.likes : ''}</b>
+//         </p>
+//         <p class="info-item">
+//         <b>Views ${item.views ? item.views : ''}</b>
+//         </p>
+//         <p class="info-item">
+//        <b>Comments ${item.comments ? item.comments : ''}</b>
+//        </p>
+//         <p class="info-item">
+//        <b>Downloads ${item.downloads ? item.downloads : ''}</b>
+//        </p>
+//       </div>
+//     </a>
+//   </div>`;
+
+// const generateContent = array =>
+//   array ? array.reduce((acc, item) => acc + createListItem(item), '') : '';
+
+// const insertContent = array => {
+//   const result = generateContent(array);
+//   refs.galleryContainer.insertAdjacentHTML('beforeend', result);
+// };
+
+// function clearGalleryContainer() {
+//   refs.galleryContainer.innerHTML = '';
+// }
+
+// let gallery = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+//   captionPosition: 'bottom',
+// });
+
+// gallery.refresh();
